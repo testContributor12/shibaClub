64eeb27ed2bab1e03b93d70396b8009eb3504f39
someone <someone@someplace.com>
2019-02-11 15:21:27 +0200

    hh

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,32 +4,12 @@ export default class Footer extends Component {
   render() {
     return (
       <div className="footerContainer">
-        <span className="messageFooter">
-          <p>
-            Sed congue sem vel condimentum sodales. Morbi dictum odio ac ex
-            sollicitudin volutpat
-          </p>
-        </span>
-
-        <div className="contactsContainer">
-          <h3 className="footerHeader">Нашите контакти</h3>
-          <div className="numberAndIcon">
-            <i className="fas fa-phone" />
-            <span className="teleNumber">+359 888 888888</span>
-          </div>
-          <div className="iconContainer">
-            <img
-              className="iconFooter iconFace"
-              src={require("../img/facebook.png")}
-              alt="facebookIcon"
-            />
-            <img
-              className="iconFooter"
-              src={require("../img/instagram.png")}
-              alt="facebookIcon"
-            />
-          </div>
-        </div>
+        <h1>
+          Уеб-сайтът е създаден за портфолиото ми, което можете да намерите
+          <span>
+            <a href="https://portfoliopavel.herokuapp.com/">ТУК</a>
+          </span>
+        </h1>
       </div>
     );
   }
